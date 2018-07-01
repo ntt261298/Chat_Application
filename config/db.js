0b3ac7dc3a96ddd1978d2a203a3d70f06d4d0009
Nguyen Tien Truong 20164289 <truong.nt164289@sis.hust.edu.vn>
2018-07-01 12:30:36 +0700

    change Procfile

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 var pg = require('pg');
 var db = new Sequelize({
-  connectionString: process.env."postgres://tmfhqkwzddbsit:760c0c78a4ba5b96489628371be15230f8af13851961eafc4f89de2b26da1e26@ec2-23-21-201-255.compute-1.amazonaws.com:5432/dfgtf76u6b4iji"
+  connectionString: process.env."postgres://tmfhqkwzddbsit:760c0c78a4ba5b96489628371be15230f8af13851961eafc4f89de2b26da1e26@ec2-23-21-201-255.compute-1.amazonaws.com:5432/dfgtf76u6b4iji",
   dialectOptions: {
     ssl: true
   },
