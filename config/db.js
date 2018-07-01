9f9d59290c3de2ae3f3925d4d238992fdb063248
Nguyen Tien Truong 20164289 <truong.nt164289@sis.hust.edu.vn>
2018-07-01 10:10:56 +0700

    add data

--- config/db.js
+++ config/db.js
@@ -1,14 +1,9 @@
 var Sequelize = require('sequelize');
 var pg = require('pg');
 var db = new Sequelize({
-  database: 'myDB',
-  username: 'postgres',
-  password: '26121998',
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
+  connectionString: process.env."postgres://tmfhqkwzddbsit:760c0c78a4ba5b96489628371be15230f8af13851961eafc4f89de2b26da1e26@ec2-23-21-201-255.compute-1.amazonaws.com:5432/dfgtf76u6b4iji"
   dialectOptions: {
-    ssl: false
+    ssl: true
   },
   operatorsAliases: false
 })
