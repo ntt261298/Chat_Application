2b8eb36ad0b76c8811c2c3a57c8bf11b53d8bb3f
Nguyen Tien Truong 20164289 <truong.nt164289@sis.hust.edu.vn>
2018-07-02 14:27:38 +0700

    change host to heroku

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 var Sequelize = require('sequelize');
 var pg = require('pg');
+var url = "postgres://tmfhqkwzddbsit:760c0c78a4ba5b96489628371be15230f8af13851961eafc4f89de2b26da1e26@ec2-23-21-201-255.compute-1.amazonaws.com:5432/dfgtf76u6b4iji";
 var db = new Sequelize({
-  database: 'myDB',
-  username: 'postgres',
-  password: '26121998',
-  host: 'localhost',
-  port: 5432,
+  connectionString: process.env.url,
   dialect: 'postgres',
   dialectOptions: {
-    ssl: false
+    ssl: true
   },
   operatorsAliases: false
 })
