8c8f476f13c516ad6ae483fd44643179fd6bc4a5
Nguyen Tien Truong 20164289 <truong.nt164289@sis.hust.edu.vn>
2018-07-10 14:44:55 +0700

    add database

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
 var Sequelize = require('sequelize');
 var pg = require('pg');
-var url = "postgres://tmfhqkwzddbsit:760c0c78a4ba5b96489628371be15230f8af13851961eafc4f89de2b26da1e26@ec2-23-21-201-255.compute-1.amazonaws.com:5432/dfgtf76u6b4iji";
-var db = new Sequelize(process.env.url, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true
-  },
-  operatorsAliases: false
-})
+var config = require('./dbURL.js');
+var db = new Sequelize({
+    database: 'dfgtf76u6b4iji',
+    username:'tmfhqkwzddbsit',
+    password:'760c0c78a4ba5b96489628371be15230f8af13851961eafc4f89de2b26da1e26',
+    host:'ec2-23-21-201-255.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  })
 
 db.authenticate()
 .then(() => console.log('Ket noi thanh cong'))
